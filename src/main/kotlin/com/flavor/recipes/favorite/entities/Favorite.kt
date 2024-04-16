bd3427dca58a96eea27fd7ac8e2fe1d6a04c41f2
lucas <lucas.cardo123@hotmail.com>
2024-04-16 17:26:02 -0400

    add name in favorite

--- src/main/kotlin/com/flavor/recipes/favorite/entities/Favorite.kt
+++ src/main/kotlin/com/flavor/recipes/favorite/entities/Favorite.kt
@@ -12,6 +12,8 @@ data class Favorite(
     var recipeId: String,
     @Column(name = "user_id", nullable = false)
     var userId: String,
+    @Column(nullable = false)
+    var name: String,
     @Column(name = "created_at", nullable = false)
     var createdAt: Long? = null,
     @Column(name = "updated_at", nullable = false)
