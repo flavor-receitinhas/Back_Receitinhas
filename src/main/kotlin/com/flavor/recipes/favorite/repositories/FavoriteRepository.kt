a1d0856abb6f61a44655e19bc6bff059df4d8494
lucas <lucas.cardo123@hotmail.com>
2024-05-05 11:03:02 -0400

    feat add search and isDesc param in favorite

--- src/main/kotlin/com/flavor/recipes/favorite/repositories/FavoriteRepository.kt
+++ src/main/kotlin/com/flavor/recipes/favorite/repositories/FavoriteRepository.kt
@@ -3,9 +3,13 @@ package com.flavor.recipes.favorite.repositories
 import com.flavor.recipes.favorite.entities.Favorite
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface FavoriteRepository : JpaRepository<Favorite, Long> {
+    @Query("Select c from Favorite c where c.name like %:search% and c.userId = :userId")
+    fun findByUserIdAndSearch(userId: String, search: String, pageable: PageRequest): List<Favorite>
     fun findByUserId(userId: String, pageable: PageRequest): List<Favorite>
+
 }
