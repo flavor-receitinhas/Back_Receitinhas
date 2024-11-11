9f6e6de77f1c19b3b45868fa3265ab6c21eacd81
lucas <lucas.cardo123@hotmail.com>
2024-11-11 16:35:16 -0400

    configure cache

--- src/main/kotlin/com/flavor/recipes/RecipesApplication.kt
+++ src/main/kotlin/com/flavor/recipes/RecipesApplication.kt
@@ -1,29 +1,37 @@
 package com.flavor.recipes
 
+import com.github.sonus21.rqueue.spring.EnableRqueue
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class RecipesApplication{
-	@PostConstruct
-	fun init() {
-		try {
-			// Inicialização do FirebaseApp
-			val options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.getApplicationDefault())
-				.setStorageBucket("receitinhas-28d4a.appspot.com")
-				.build()
-			FirebaseApp.initializeApp(options)
-		} catch(e: Exception){
-			print(e)
-		}
-	}
+@EnableCaching
+@EnableRqueue
+class RecipesApplication {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun init() {
+        try {
+            // Inicialização do FirebaseApp
+            val options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setStorageBucket("receitinhas-28d4a.appspot.com")
+                .build()
+            FirebaseApp.initializeApp(options)
+        } catch (e: Exception) {
+            log.error("Exception", e)
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<RecipesApplication>(*args)
+    runApplication<RecipesApplication>(*args)
 }
